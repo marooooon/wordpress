4efea74a5d8dc5f6a025bc5b42bfc3007b8c3db9
Daijiro Takeuchi <mr.daijiro0517@gmail.com>
2019-09-08 16:13:06 +0900

    add wordpress for docker

---
+++ myblog/wp-content/themes/daijiroblog/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>daijiroblog</title>
+    <link rel="stylesheet"  href="<?php  echo get_template_directory_uri(); ?>/css/style.css" type="text/css" >
+    <link rel="stylesheet"  href="<?php echo get_template_directory_uri(); ?>/css/responsive.css" type="text/css">
+</head>
+<body>
+<p>テスト</p>
+<img src="<?php echo get_template_directory_uri(); ?>/img/zzz.png">
+</body>
+</html>
